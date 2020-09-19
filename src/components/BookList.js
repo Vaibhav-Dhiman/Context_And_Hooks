54011a8b1626c939626df9a69a0de89b85dde2a1
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-19 20:48:12 +0530

    cleaning,BooList and Navbar added

---
+++ src/components/BookList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class BookList extends Component {
+       render() { 
+        return (  
+            <div className = "book-list">
+                <li>dw</li>
+                <li>fr</li>
+                <li>th</li>
+            </div>
+        );
+    }
+}
+ 
+export default BookList;
