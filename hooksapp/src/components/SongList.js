db37c833726266b8cb6b8665a1665ff5ea212668
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-20 21:15:34 +0530

    songlist added

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+const SongList = () => {
+    return (  
+        <div className="song-list">
+            <ul>
+                <li>this is song</li>
+                <li>this is song efu</li>
+            </ul>
+        </div>
+    );
+}
+ 
+export default SongList;
