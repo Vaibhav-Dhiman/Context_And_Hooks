50c3d49901dc90c524aeb4e57f7751cfdac9ba16
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-20 15:09:52 +0530

    sharing data using context

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
+import {ThemeContext} from '../contexts/ThemeContext';
 
 class BookList extends Component {
+  static contextType = ThemeContext;
        render() { 
+          const {isLightTheme,light,dark} = this.context;
+          const theme = isLightTheme ? light : dark;
         return (  
-            <div className = "book-list">
-                <li>dw</li>
-                <li>fr</li>
-                <li>th</li>
+            <div className = "book-list" style={{color: theme.syntax, background: theme.bg}}>
+                <li style={{background: theme.ui}}>dw</li>
+                <li style={{background: theme.ui}}>fr</li>
+                <li style={{background: theme.ui}}>th</li>
             </div>
         );
     }
