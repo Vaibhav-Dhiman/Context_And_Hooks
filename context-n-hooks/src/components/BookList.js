ab5b759ce358aae0964243f244893ea08fa9f94c
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-21 12:06:04 +0530

    useContext added in BookList

--- context-n-hooks/src/components/BookList.js
+++ context-n-hooks/src/components/BookList.js
@@ -1,20 +1,32 @@
-import React, { Component } from 'react';
+import React, { Component, useContext } from 'react';
 import {ThemeContext} from '../contexts/ThemeContext';
 
-class BookList extends Component {
+//class BookList extends Component {
     // below using context type to get data from the context , can also use context consumer
-  static contextType = ThemeContext;
-       render() { 
-          const {isLightTheme,light,dark} = this.context;
-          const theme = isLightTheme ? light : dark;
-        return (  
-            <div className = "book-list" style={{color: theme.syntax, background: theme.bg}}>
-                <li style={{background: theme.ui}}>dw</li>
-                <li style={{background: theme.ui}}>fr</li>
-                <li style={{background: theme.ui}}>th</li>
-            </div>
-        );
-    }
+//   static contextType = ThemeContext;
+//        render() { 
+//           const {isLightTheme,light,dark} = this.context;
+//           const theme = isLightTheme ? light : dark;
+//         return (  
+//             <div className = "book-list" style={{color: theme.syntax, background: theme.bg}}>
+//                 <li style={{background: theme.ui}}>dw</li>
+//                 <li style={{background: theme.ui}}>fr</li>
+//                 <li style={{background: theme.ui}}>th</li>
+//             </div>
+//         );
+//     }
+// }
+
+const BookList = () => {
+    const {isLightTheme, light, dark} = useContext(ThemeContext);
+    const theme = isLightTheme ? light : dark;
+    return (  
+        <div className = "book-list" style={{color: theme.syntax, background: theme.bg}}>
+            <li style={{background: theme.ui}}>dw</li>
+            <li style={{background: theme.ui}}>fr</li>
+            <li style={{background: theme.ui}}>th</li>
+       </div>
+    );
 }
  
 export default BookList;
