aa14d6d3f9585b9203d20b0dc778d28aff7a83ca
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-23 16:29:33 +0530

    making context and counsuimg in navbar

---
+++ booklist/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { useContext } from 'react';
+import { BookContext } from '../context/BookContext';
+
+const Navbar = () => {
+    const {books} = useContext(BookContext);
+    return (
+        <div className="navbar">
+            <h1>Ninja Reading List</h1>
+            <p>Currently You Hvae {books.length} books to get through</p>
+        </div>
+    );
+}
+export default Navbar;
--- booklist/src/context/BookContext.js
+++ booklist/src/context/BookContext.js
@@ -1,16 +1,17 @@
-import React, { createContext,useState,useContext } from 'react';
+import React, { createContext,useState } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 
 export const BookContext = createContext();
+
 const BookContextProvider = (props) => {
-    const [books, setBooks] =useState([
+    const [books, setBooks] = useState([
         {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
         {title: 'the final empire', author: 'brandon sanderson', id: 2},
         {title: 'monk who sold his ferrari', author: 'robin sharma', id: 3}
     ]);
 
-    const adBook = (title,author) => {
-            setBooks([...books, {title,author,id:uuidv4()}]);
+    const addBook = (title,author) => {
+            setBooks([...books, { title, author, id:uuidv4() }]);
     };
 
     const removeBook = (id) => {
@@ -18,7 +19,7 @@ const BookContextProvider = (props) => {
     };
 
     return (
-        <BookContext.Provider value = {books,adBook,removeBook}>
+        <BookContext.Provider value = {{books,addBook,removeBook}}>
             {props.children}
         </BookContext.Provider>
     )
