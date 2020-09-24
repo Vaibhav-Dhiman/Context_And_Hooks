b874e47f1881f819e2d8fd26cd177576febc0606
vaibhav dhiman <princedhiman70@gmail.com>
2020-09-24 10:34:53 +0530

    reducers added for add and remove

--- booklist/src/context/BookContext.js
+++ booklist/src/context/BookContext.js
@@ -1,25 +1,13 @@
-import React, { createContext,useState } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import React, { createContext, useReducer } from 'react';
+import { bookReducers } from '../reducers/bookReducers';
 
 export const BookContext = createContext();
 
 const BookContextProvider = (props) => {
-    const [books, setBooks] = useState([
-        {title: 'name of the wind', author: 'patrick rothfuss', id: 1},
-        {title: 'the final empire', author: 'brandon sanderson', id: 2},
-        {title: 'monk who sold his ferrari', author: 'robin sharma', id: 3}
-    ]);
-
-    const addBook = (title,author) => {
-            setBooks([...books, { title, author, id:uuidv4() }]);
-    };
-
-    const removeBook = (id) => {
-        setBooks(books.filter(book => book.id !== id));
-    };
+    const [books, dispatch] = useReducer(bookReducers, []);
 
     return (
-        <BookContext.Provider value = {{books,addBook,removeBook}}>
+        <BookContext.Provider value = {{books, dispatch}}>
             {props.children}
         </BookContext.Provider>
     )
